c78e6288e82f3c85375b64261fdc4a614489e49c
angelmarques <angel.jose.marques@estudiantat.upc.edu>
2023-05-31 21:49:56 +0200

    commit 0

---
+++ client/config.py
@@ -0,0 +1,11 @@
+# Configuration file used for client node when running on local
+from os import environ
+
+DEFAULT_SERVER_URL = 'http://127.0.0.1:5000'
+GLOBAL_TMP_PATH = '/tmp'
+GLOBAL_DATASETS = '/Users/angelmarques/tfg/federated-learning-network/datasets'
+
+
+
+
+
